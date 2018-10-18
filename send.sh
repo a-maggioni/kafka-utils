2a3fa8510e29bf1fea25a80556a455dbb25e3183
Alberto Maggioni <maggionialberto@gmail.com>
2018-10-18 10:26:10 +0200

    Add key to partition data

--- send.sh
+++ send.sh
@@ -1,1 +1,1 @@
-cat stocks.txt | docker exec -i $(docker ps -aqf "name=kafka") kafka-console-producer.sh --broker-list localhost:9092 --topic Stocks
+cat stocks.txt | docker exec -i $(docker ps -aqf "name=kafka") kafka-console-producer.sh --broker-list localhost:9092 --topic Stocks --property "parse.key=true" --property "key.separator=:"
